819809d17b2f390b8a50374d1aa1f98445618f66
Mohammad Riftadi <riftadi@gmail.com>
2018-03-28 14:56:34 +0200

    Change IP to AWS servers.

--- battle_dragons.sh
+++ battle_dragons.sh
@@ -4,8 +4,8 @@
 # python -m client.Observer 127.0.0.1:9191 &
 # python -m client.Observer 127.0.0.1:7191 &
 
-python -m client.Observer 34.244.190.223:8181 &
-python -m client.Observer 52.209.25.212:8181 &
+python -m client.Observer 34.245.150.134:8181 &
+python -m client.Observer 34.244.251.145:8181 &
 
 for run in {1..20}
 do
